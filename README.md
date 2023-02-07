8e9371c29dadcbe17d9eccd88b5d8d1e5124c20e
Mars <mars.hu@airwallex.com>
2023-02-07 09:28:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Calibre CBZ Convertor
+Plugin to convert comic book to cbz (zip) format
+
+# How to install
+
+(1) in `Terminal.app` or your prefer command line terminal
+
+```bash
+git clone https://github.com/huyumars/calibre-cbz-convertor.git
+cd calibre-cbz-convertor
+make zip
+```
+
+You will be able to locate a `cbz.zip` in the folder, please remember the path
+
+(2) launch the `calibre` application
+
+(3) launch the `Prefernces` page, click the `Plugins` icon
+
+<img src="img/2.jpg" >
+
+(4) click the `Load plugin from file` button, and choose `cbz.zip` from step (1)
+
+<img src="img/3.jpg" >
+
+(5) enjoy!
---
+++ cbz/__init__.py
@@ -0,0 +1,23 @@
+from calibre.customize.conversion import OutputFormatPlugin
+from calibre.ptempfile import TemporaryDirectory
+from calibre.utils.zipfile import ZipFile
+from os import path
+
+
+class MarkdownOutput(OutputFormatPlugin):
+    name = "CBZ Output"
+    author = "mars"
+    version = (0, 0, 1)
+    file_type = "cbz"
+    commit_name = "cbz_output"
+
+    def convert(self, oeb_book, output_path, input_plugin, opts, log):
+        cbz = ZipFile(output_path, mode="w")
+        with TemporaryDirectory('_img_extract_') as tdir:
+            for item in oeb_book.manifest:
+                if item.media_type.startswith('image'):
+                    filename = tdir + "/" + path.basename(item.href)
+                    with open(filename, "wb") as binary_file:
+                        # Write bytes to file
+                        binary_file.write(item.data)
+                    cbz.write(filename, path.basename(item.href))
